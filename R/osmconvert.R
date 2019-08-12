640c029492b5322d6f4f0095187a3a7b9e9e032f
Malcolm Morgan <mem48@users.noreply.github.com>
2019-08-12 12:22:25 +0100

    working package

--- R/osmconvert.R
+++ R/osmconvert.R
@@ -4,15 +4,29 @@
 #'  @param format format to convert to
 #' 
 #' 
-osmt_convert <- function(file, format_out = "o5m"){
+osmt_convert <- function(file, format_out = "o5m", drop_author = TRUE){
   checkmate::assert_os("windows")
   checkmate::assert_choice(format_out, 
                            choice = c("osm","osc",
                                       "osc.gz","osh",
                                       "o5m","o5c","pbf"))
+  file <- normalizePath(file)
   checkmate::assert_file_exists(file)
-  path.package("OSMTools")
+  path_out <- paste0(substr(file,1,nchar(file)-3),format_out)
+  #checkmate::check_file_exists(path_out)
+  path_osmconvert <- file.path(path.package("OSMtools"),"osmconvert.exe")
+  path_osmconvert <- normalizePath(path_osmconvert)
+  request <- paste0(path_osmconvert, 
+                    " ",
+                    file,
+                    " --out-",
+                    format_out)
+  if(drop_author){
+    request <- paste0(request," --drop-author")
+  }
+  request <- paste0(request," -o=",path_out)
   
+  system(request, intern = TRUE)
   
   
 }
---
+++ R/osmfilter.R
@@ -0,0 +1,54 @@
+#' Filter osm by tags
+#' 
+#'  @param file path to file
+#'  @param path_out path to file to save result
+#'  @param keep object to keep arguments
+#'  @param drop object to drop arguments
+#'  @param keep_tags tags to keep arguments
+#'  @param drop_tags togs to drop arguments
+#' 
+#' 
+
+osmt_filter <- function(file, path_out,
+                                keep = NULL, drop = NULL,
+                                keep_tags = NULL,
+                                drop_tags = NULL){
+  checkmate::assert_os("windows")
+  file <- normalizePath(file)
+  checkmate::assert_file_exists(file)
+  path_osmfilter <- file.path(path.package("OSMtools"),"osmfilter.exe")
+  path_osmfilter <- normalizePath(path_osmfilter)
+  
+  request <- paste0(path_osmfilter," ",file)
+  if(!is.null(keep)){
+    add <- paste0(' --keep="',
+                  keep,
+                  '"')
+    request <- paste0(request,add)
+  }
+  if(!is.null(drop)){
+    request <- paste0(request,
+                      ' --drop="',
+                      drop,
+                      '"'
+                      )
+  }
+  if(!is.null(keep_tags)){
+    request <- paste0(request,
+                      ' --keep-tags="',
+                      keep_tags,
+                      '"')
+  }
+  if(!is.null(drop_tags)){
+    request <- paste0(request,
+                      ' --drop-tags="',
+                      drop_tags,
+                      '"')
+  }
+  
+  path_out <- normalizePath(path_out)
+  request <- paste0(request," -o=",path_out)
+  
+  system(request, intern = TRUE)
+  
+}
